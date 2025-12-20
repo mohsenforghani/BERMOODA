11df52b33629c66b42d313ad982f2ba857d52f9b
Mohsen <152495454+mohsenforghani@users.noreply.github.com>
2025-12-20 13:47:21 +0330

    Update boss.js

--- js/boss.js
+++ js/boss.js
@@ -15,7 +15,7 @@
       y: -200,
       w: 60,
       h: 100,
-      life: 1500,
+      life: 100,
       targetX: canvas.width / 2 - 100,
       targetY: canvas.height / 2 - 100,
       speed: 1,
@@ -215,4 +215,5 @@ return BossAPI;
 
 })();
 window.Boss = Boss;
+
 
