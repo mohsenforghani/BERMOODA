5f06f569820e0730f2e9e516065cf56699aa93ed
Mohsen <152495454+mohsenforghani@users.noreply.github.com>
2026-01-05 18:51:03 +0330

    sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "bermooda-cache-v5"; // با هر آپدیت تغییر دهید
+const CACHE_NAME = "bermooda-cache-v3"; // هر نسخه جدید، شماره نسخه را تغییر دهید
 const ASSETS = [
 "/BERMOODA/",
 
@@ -108,12 +108,11 @@ const ASSETS = [
   "/BERMOODA/index.html"
 ];
 
-// نصب SW و کش فایل‌ها
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
-  // self.skipWaiting(); <-- حذف شد
+
 });
 
 // فعال‌سازی و حذف کش‌های قدیمی
@@ -128,18 +127,17 @@ self.addEventListener("activate", event => {
   self.clients.claim();
 });
 
-// پاسخ به fetch
+// fetch از کش یا شبکه
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(cached => cached || fetch(event.request))
   );
 });
 
-// گوش دادن به پیام‌ها برای فعال‌سازی SW جدید
+// گوش دادن به پیام از صفحه (برای فعال‌سازی نسخه جدید)
 self.addEventListener("message", event => {
   if (event.data === "SKIP_WAITING") {
     self.skipWaiting();
   }
 });
-
 
