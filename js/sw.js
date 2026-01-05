bd2ef947ad388bbc0559dd87d156bd10655962e0
Mohsen <152495454+mohsenforghani@users.noreply.github.com>
2026-01-05 17:35:55 +0330

    sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "bermooda-cache-v2"; // با هر آپدیت تغییر دهید
+const CACHE_NAME = "bermooda-cache-v4"; // با هر آپدیت تغییر دهید
 const ASSETS = [
 "/BERMOODA/",
 
@@ -141,3 +141,4 @@ self.addEventListener("message", event => {
     self.skipWaiting();
   }
 });
+
